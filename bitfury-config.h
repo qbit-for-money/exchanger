5b28d8a4b5b4542e3b3915f1af9c1e26fc531756
root <root@raspberrypi.(none)>
2013-09-13 07:34:27 +0000

    added bitfury-config.h

---
+++ bitfury-config.h
@@ -0,0 +1,42 @@
+/**
+ * libbitfury.h - library for Bitfury chip/board
+ *
+ * Copyright (c) 2013 needbmw
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+**/
+
+#ifndef __BITFURYCONFIG_H__
+#define __BITFURYCONFIG_H__
+
+/* Select your type of hardware */
+
+//#define BITFURY_METABANK 1
+#define BITFURY_NEEDBMW_NOMUX 1
+
+
+/* Set stats options */
+
+//#define BITFURY_ENABLE_SHORT_STAT 1
+//#define BITFURY_ENABLE_LONG_STAT 1
+#define BITFURY_API_STATS 300
+#define BITFURY_STAT_N 1024
+
+#endif
