38cb6bc8f48c0ba1e8f59e3b4781989a313e3ecf
DiscoElevator <maxdoom@mail.ru>
2014-02-17 11:13:47 +0400

    refactoring

--- src/main/webapp/resources/js/wizard/wizard.js
+++ src/main/webapp/resources/js/wizard/wizard.js
@@ -1,13 +1,5 @@
 var wizardModule = angular.module("wizard");
 
 wizardModule.controller("WizardController", function($scope, $rootScope) {
-	$rootScope.orderInfo = {
-		inTransfer: {
-			type: "IN"
-		},
-		outTransfer: {
-			type: "OUT"
-		}
-	};
 });
 
