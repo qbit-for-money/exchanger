dffd8289091ced726e89d365a50c1c122e842f75
DiscoElevator <maxdoom@mail.ru>
2014-02-24 15:39:33 +0400

    yandex props

--- src/main/java/com/qbit/exchanger/money/yandex/YandexMoneyService.java
+++ src/main/java/com/qbit/exchanger/money/yandex/YandexMoneyService.java
@@ -1,5 +1,6 @@
 package com.qbit.exchanger.money.yandex;
 
+import com.qbit.exchanger.env.Env;
 import com.qbit.exchanger.money.core.MoneyService;
 import com.qbit.exchanger.money.core.MoneyTransferCallback;
 import com.qbit.exchanger.money.model.Transfer;
@@ -13,6 +14,7 @@ import java.util.Map;
 import java.util.StringTokenizer;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.logging.Logger;
+import javax.inject.Inject;
 import javax.inject.Singleton;
 import ru.yandex.money.api.YandexMoney;
 import ru.yandex.money.api.YandexMoneyImpl;
@@ -34,15 +36,13 @@ public class YandexMoneyService implements MoneyService {
 
 	private static final Logger LOGGER = Logger.getLogger(YandexMoneyService.class.getName());
 	private static final String CLIENT_ID = "B83AF6B23CA9C5E0CA7AAFC2F1B98CDAEEAD59A49DED9A4BEE52B8F85A19D20B";
-	private static final String REDIRECT_URI = "https://localhost:8443/exchanger/webapi/yandex/proceedAuth";
-	private static final String STORE_WALLET = "41001954722279";
-	private static final String STORE_TOKEN = "41001954722279.41FA1CFDB8228302CA23314BFEF423E2A3A719994AFE60A5"
-			+ "6CB63DC438FE08B94CD8AFFC231EEE9404A19F0943D1D2B15E211561AD73A899951C62FE0C9891A761641F089F9C57D0"
-			+ "6D5955FCDDB2DA609F6B7986E09EDFDFA580C5F543F4EA2091093BE448338B4D5E93D58D3F484BB804342607C48D1B7C5DF9AF1A86B271C6";
-	private static final String OPERATION_DESCRIPTION = "test";
+	private static final String OPERATION_DESCRIPTION = "exchanger";
 
 	private final YandexMoney yandexMoney;
 	private final Map<String, String> tokens;
+
+	@Inject
+	private Env env;
 
 	public YandexMoneyService() {
 		tokens = new ConcurrentHashMap<>();
@@ -51,17 +51,17 @@ public class YandexMoneyService implements MoneyService {
 
 	public String getAuthorizeUri(Boolean mobile, BigDecimal amount) {
 		Collection<Permission> scope = getPaymentScope(amount);
-		return yandexMoney.authorizeUri(scope, REDIRECT_URI, mobile);
+		return yandexMoney.authorizeUri(scope, env.getYandexRedirectUrl(), mobile);
 	}
 
 	public String getAuthorizeUri(Boolean mobile) {
 		Collection<Permission> scope = getPaymentScope();
-		return yandexMoney.authorizeUri(scope, REDIRECT_URI, mobile);
+		return yandexMoney.authorizeUri(scope, env.getYandexRedirectUrl(), mobile);
 	}
 
 	public BigDecimal getBalance() throws RuntimeException {
 		try {
-			AccountInfoResponse response = yandexMoney.accountInfo(STORE_TOKEN);
+			AccountInfoResponse response = yandexMoney.accountInfo(env.getYandexToken());
 			return response.getBalance();
 		} catch (Exception e) {
 			throw new RuntimeException(e.getMessage());
@@ -74,9 +74,9 @@ public class YandexMoneyService implements MoneyService {
 		String token;
 		if (TransferType.IN.equals(transfer.getType())) {
 			token = tokens.get(transfer.getAddress());
-			wallet = STORE_WALLET;
+			wallet = env.getYandexWallet();
 		} else {
-			token = STORE_TOKEN;
+			token = env.getYandexToken();
 			wallet = transfer.getAddress();
 		}
 
@@ -109,9 +109,9 @@ public class YandexMoneyService implements MoneyService {
 			String token;
 			if (TransferType.IN.equals(transfer.getType())) {
 				token = tokens.get(transfer.getAddress());
-				wallet = STORE_WALLET;
+				wallet = env.getYandexWallet();
 			} else {
-				token = STORE_TOKEN;
+				token = env.getYandexToken();
 				wallet = transfer.getAddress();
 			}
 
@@ -141,7 +141,7 @@ public class YandexMoneyService implements MoneyService {
 	private String exchangeToken(String code) throws RuntimeException {
 		String token = null;
 		try {
-			ReceiveOAuthTokenResponse tokenResponse = yandexMoney.receiveOAuthToken(code, REDIRECT_URI);
+			ReceiveOAuthTokenResponse tokenResponse = yandexMoney.receiveOAuthToken(code, env.getYandexRedirectUrl());
 			if (tokenResponse != null && tokenResponse.isSuccess()) {
 				token = tokenResponse.getAccessToken();
 			}
@@ -202,13 +202,13 @@ public class YandexMoneyService implements MoneyService {
 	}
 
 	private Collection<Permission> getPaymentScope(BigDecimal sum) {
-		Permission scope = new Payment(Destination.toAccount, STORE_WALLET, sum.toString());
+		Permission scope = new Payment(Destination.toAccount, env.getYandexWallet(), sum.toString());
 		return Collections.singletonList(scope);
 	}
 
 	private Collection<Permission> getPaymentScope() {
 		Payment scope = new Payment();
-		scope.toAccount(STORE_WALLET);
+		scope.toAccount(env.getYandexWallet());
 		Permission result = scope;
 		return Collections.singletonList(result);
 	}
