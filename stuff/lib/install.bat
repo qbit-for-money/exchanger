33b474cceda9063e12367b04eb778db31e8c8222
SanekGit <aleksashka-666@yandex.ru>
2014-02-27 11:28:42 +0400

    set litecoinj version 0.11

--- stuff/lib/install.bat
+++ stuff/lib/install.bat
@@ -1,2 +1,2 @@
 mvn "install:install-file" "-DgroupId=ru.yandex.money" "-DartifactId=yamolib" "-Dversion=1.2.4-SNAPSHOT" "-Dpackaging=jar" "-Dfile=yamolib.jar" "-DgeneratePom=true"
-mvn "install:install-file" "-DgroupId=com.google" "-DartifactId=litecoinj" "-Dversion=0.10.3" "-Dpackaging=jar" "-Dfile=litecoinj.jar" "-DgeneratePom=true"
+mvn "install:install-file" "-DgroupId=com.google" "-DartifactId=litecoinj" "-Dversion=0.11" "-Dpackaging=jar" "-Dfile=litecoinj.jar" "-DgeneratePom=true"
