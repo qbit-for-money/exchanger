90ecb695171f7c3689363e51f2dbfb0cb0a571ad
Alexander Alexandrov <Alexander_Alexandrov@epam.com>
2014-04-26 11:12:51 +0400

    captcha auth key rotation

--- src/main/java/com/qbit/exchanger/auth/AuthKey.java
+++ src/main/java/com/qbit/exchanger/auth/AuthKey.java
@@ -10,30 +10,55 @@ import java.util.Random;
  * @author Alexander_Alexandrov
  */
 public final class AuthKey implements Serializable {
-	
-	private static final BigInteger CHIPHER_KEY = new BigInteger("f37uq9ybyk1a", Character.MAX_RADIX);
-	
+
+	private static final int PIN_BIT_LEN = 16;
+	private static final BigInteger PIN_MASK = BigInteger.valueOf(2).pow(PIN_BIT_LEN).subtract(BigInteger.ONE);
+	private static final int PIN_MAX_BIT_INDEX = PIN_BIT_LEN - 1;
+
+	private static final int MAX_SHIFT = 32;
+	private static final BigInteger MAX_SHIFT_MASK = BigInteger.valueOf(MAX_SHIFT - 1);
+	private static final int MAX_SHIFT_BIT_LEN = BigInteger.valueOf(MAX_SHIFT).bitLength();
+
 	private static final Random rnd = new Random();
-	
+
 	public static String encode(AuthKey key) {
-		BigInteger pin = new BigInteger(key.pin).setBit(15);
+		BigInteger pin = new BigInteger(key.pin).setBit(PIN_MAX_BIT_INDEX);
 		BigInteger timestamp = BigInteger.valueOf(key.timestamp);
-		BigInteger result = BigInteger.ZERO.add(pin)
-				.shiftLeft(timestamp.bitLength()).add(timestamp);
-		return result.shiftLeft(4).add(BigInteger.valueOf(rnd.nextInt(0x10)))
-				.xor(CHIPHER_KEY).toString(Character.MAX_RADIX);
+		BigInteger pinAndTimestamp = timestamp.shiftLeft(PIN_BIT_LEN).or(pin);
+		int shift = rnd.nextInt(MAX_SHIFT);
+		BigInteger rotatedKey = rotateLeft(pinAndTimestamp, pinAndTimestamp.bitLength(), shift)
+				.setBit(pinAndTimestamp.bitLength());
+		BigInteger rotatedKeyAndShift = rotatedKey.shiftLeft(MAX_SHIFT_BIT_LEN).or(BigInteger.valueOf(shift));
+		return rotatedKeyAndShift.toString(Character.MAX_RADIX);
 	}
-	
-	public static AuthKey decode(String encodedKey) {
-		BigInteger decryptedKey = new BigInteger(encodedKey, Character.MAX_RADIX).xor(CHIPHER_KEY).shiftRight(4);
-		BigInteger pin = decryptedKey.shiftRight(decryptedKey.bitLength() - 16).clearBit(15);
-		BigInteger timestamp = decryptedKey.subtract(pin.setBit(15).shiftLeft(decryptedKey.bitLength() - 16));
+
+	public static AuthKey decode(String encriptedKey) {
+		BigInteger rotatedKeyAndShift = new BigInteger(encriptedKey, Character.MAX_RADIX);
+		int shift = rotatedKeyAndShift.and(MAX_SHIFT_MASK).intValue();
+		BigInteger rotatedKey = rotatedKeyAndShift.shiftRight(MAX_SHIFT_BIT_LEN);
+		int pinAndTimestampBitLen = (rotatedKey.bitLength() - 1);
+		BigInteger pinAndTimestamp = rotateRight(rotatedKey.clearBit(pinAndTimestampBitLen),
+				pinAndTimestampBitLen, shift);
+		BigInteger pin = pinAndTimestamp.and(PIN_MASK).clearBit(PIN_MAX_BIT_INDEX);
+		BigInteger timestamp = pinAndTimestamp.shiftRight(PIN_BIT_LEN);
 		return new AuthKey(pin.toString(), timestamp.longValue());
 	}
-	
+
+	private static BigInteger rotateLeft(BigInteger num, int bitLength, int shift) {
+		return num.shiftLeft(shift).or(num.shiftRight(bitLength - shift)).and(ones(bitLength));
+	}
+
+	private static BigInteger rotateRight(BigInteger num, int bitLength, int shift) {
+		return num.shiftRight(shift).or(num.shiftLeft(bitLength - shift)).and(ones(bitLength));
+	}
+
+	private static BigInteger ones(int bitLength) {
+		return BigInteger.valueOf(2).pow(bitLength).subtract(BigInteger.ONE);
+	}
+
 	private final String pin;
 	private final long timestamp;
-	
+
 	protected AuthKey() {
 		this("0000", 0L);
 	}
@@ -84,17 +109,22 @@ public final class AuthKey implements Serializable {
 	public String toString() {
 		return "AuthKey{" + "pin=" + pin + ", timestamp=" + timestamp + '}';
 	}
-	
+
 	/**
 	 * Unit test
+	 * @param args
 	 */
 	public static void main(String[] args) {
-		AuthKey authKey = new AuthKey("1234", System.currentTimeMillis());
+		AuthKey authKey = new AuthKey("1234", 123456789);
 		System.out.println(authKey);
-		String encodedKey = AuthKey.encode(authKey);
-		System.out.println(encodedKey);
-		AuthKey decodedKey = AuthKey.decode(encodedKey);
-		System.out.println(decodedKey);
-		assert authKey.equals(decodedKey);
+		for (int i = 0; i < 100; i++) {
+			String encodedKey = AuthKey.encode(authKey);
+			System.out.println(encodedKey);
+			AuthKey decodedKey = AuthKey.decode(encodedKey);
+			if (!authKey.equals(decodedKey)) {
+				System.out.println("Error: " + decodedKey);
+				break;
+			}
+		}
 	}
 }
